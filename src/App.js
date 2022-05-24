334117cd0c3792a6612a4a4264bcbc2142218525
Mohammad Parvez Musharaf <mdparvezmusharaf2@gail.com>
2022-05-24 12:25:56 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, Fragment } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) =>{
+        setUsersList((prevUserList) => {
+          return [...prevUserList, {name: uName, age: uAge}];
+        })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+          <AddUser onAddUser={addUserHandler} />
+          <UserList users ={usersList} />
+    </Fragment>
   );
 }
 
